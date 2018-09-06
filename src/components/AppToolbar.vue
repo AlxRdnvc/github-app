b48da5a76934a56eced8348c37ebc4c4f37cefb9
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-09-06 19:55:23 +0200

    zadatak 2

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="left">
+                <slot>LEFT</slot>
+            </div>
+            <div class="center">
+                <h2>{{title}}</h2>
+            </div>
+            <div class="right">
+                <slot>RIGHT</slot>
+            </div>
+        </v-ons-toolbar>
+    </v-ons-page>
+</template>
+
+<script>
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Github App'
+    }
+  }
+}
+</script>

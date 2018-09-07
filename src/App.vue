b62e5b8c1b31d037eb5b463b4be6be881e5da417
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-09-07 14:05:57 +0200

    zadatak 5

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <v-ons-page>
-      <AppToolbar />
-      <div class="center">
-        <AppSearch :query.sync="query"/>
-      </div>
-    </v-ons-page>
+  <v-ons-page>
+
+    <AppToolbar />
+
+    <div class="center">
+      <AppSearch :query.sync="query"/>
+    </div>
+    
+  </v-ons-page>
 </template>
 
 <script>
   import AppToolbar from './components/AppToolbar.vue'
   import AppSearch from './components/AppSearch.vue'
+  import debounce from 'lodash/debounce'
+  import { gitHub } from './services/GitHub.js'
 
   export default{
   components: {
@@ -18,7 +23,21 @@
   },
   data(){
     return {
-      query:''
+      query:'',
+      repos: ''
+    }
+  },
+  methods: {
+    getRepos: debounce(function () {
+      gitHub.getRepos(this.query)
+        .then((response) => {
+          this.repos = response.data
+        })
+    }, 500)
+  },
+   watch: {
+    query (value) {
+      this.getRepos(value)
     }
   }
 };
--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,15 +1,23 @@
 <template>
-        <v-ons-toolbar>
-            <div class="left">
-                <slot name="left">LEFT</slot>
-            </div>
-            <div class="center" style="text-align: center;">
-                {{title}}
-            </div>
-            <div class="right">
-                <slot name="right">RIGHT</slot>
-            </div>
-        </v-ons-toolbar>
+    <v-ons-toolbar>
+        <div class="left">
+            <slot name="left">
+                <v-ons-button>
+                    <v-ons-icon icon="md-menu"></v-ons-icon>
+                </v-ons-button>
+            </slot>
+        </div>
+        <div class="center" style="text-align: center;">
+            {{title}}
+        </div>
+        <div class="right">
+            <slot name="right">
+                    <v-ons-button>
+                        <v-ons-icon icon="md-settings"></v-ons-icon>
+                    </v-ons-button>
+            </slot>
+        </div>
+    </v-ons-toolbar>
 </template>
 
 <script>
